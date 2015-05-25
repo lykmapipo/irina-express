92d44acff56fa9023d4ea12154dda0e6cc1728e4
lykmapipo <lallyelias87@gmail.com>
2015-05-25 15:02:00 +0300

    initializing irina-express

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//dependencies
+var _ = reuqire('lodash');
+var express = require('express');
+
+/**
+ * @function
+ * @description irina express based authentication workflows
+ * @param  {Object} options valid irina express options
+ * @public
+ */
+module.exports = exports = function IrinaExpress(options) {
+    options = options || {};
+};
