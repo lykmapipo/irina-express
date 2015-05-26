ce2b45356b206ec3a6f059a985a9aecb266d85dd
lykmapipo <lallyelias87@gmail.com>
2015-05-26 21:05:58 +0300

    taking care of existing credentials

--- test/unit/signup.spec.js
+++ test/unit/signup.spec.js
@@ -2,6 +2,7 @@
 
 //dependencies
 var path = require('path');
+var _ = require('lodash');
 var expect = require('chai').expect;
 var Request = require('mock-express-request');
 var Response = require('mock-express-response');
@@ -11,6 +12,7 @@ var User = mongoose.model('User');
 
 //signup logics
 var signup = require(path.join(__dirname, '..', '..', 'lib', 'signup'))(User);
+var _user_;
 
 describe('Signup', function() {
 
@@ -29,6 +31,9 @@ describe('Signup', function() {
             finish: function() {
                 var _user = response._getJSON();
 
+                //remember user
+                _user_ = _user;
+
                 expect(response.statusCode).to.equal(201);
 
                 expect(_user.email).to.equal(user.email);
@@ -41,7 +46,7 @@ describe('Signup', function() {
     });
 
 
-    it('should be able to return validation error if valid email is not provided', function(done) {
+    it('should be able to return validation error if valid credentials are not provided', function(done) {
         var user = {
             email: '',
             password: ''
@@ -56,9 +61,39 @@ describe('Signup', function() {
             finish: function() {
                 var _response = response._getJSON();
 
-                console.log(_response);
+                expect(response.statusCode).to.equal(400);
+                expect(_response.code).to.equal(400);
+                expect(_response.name).to.equal('ValidationError');
+
+                expect(_response.errors.password).to.exist;
+                expect(_response.errors.email).to.exist;
+
+                done();
+            }
+        });
+
+        signup(request, response);
+    });
+
+
+    it('should be able to return unique error if user with same credentials exist', function(done) {
+        var user = _.clone(_user_);
+        user.password = faker.internet.password();
+
+        var request = new Request({
+            body: user
+        });
+
+        var response = new Response({
+            request: request,
+            finish: function() {
+                var _response = response._getJSON();
 
                 expect(response.statusCode).to.equal(400);
+                expect(_response.code).to.equal(400);
+                expect(_response.name).to.equal('ValidationError');
+
+                expect(_response.errors.email).to.exist;
 
                 done();
             }
